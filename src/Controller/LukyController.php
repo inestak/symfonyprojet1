b9c676761cccd1871c9a8882ce75d6934352f547
Ines <ines.taktak2512@gmail.com>
2021-11-07 12:08:52 +0100

    initial commit

---
+++ src/Controller/LukyController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LukyController extends AbstractController
+{
+    /**
+     * @Route("/luky", name="luky")
+     */
+    public function index(): Response
+    {
+        return $this->render('luky/index.html.twig', [
+            'controller_name' => 'LukyController',
+            'number'=> '$number' ,
+
+        ]);
+    }
+
+    /**
+     * @Route("/luky/number", name="lukynumber")
+     */
+    public function number(): Response
+    {
+     $number=random_int(0,100);
+     return new Response("<h1>Lucky number: {$number} </h1>"); 
+    }
+
+}
